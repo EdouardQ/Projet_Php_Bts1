d8e7135fa6407f671b368b5b2b6ea20b0b1e1236
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-07 14:41:04 +0100

    Main

---
+++ Html-Php/Accueil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Les Rousses - Accueil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./design.css" media="all">
+</head>
+<body>
+	<header>
+		<a href=".\accueil.php"><img src="..\images\Logo-les-rousses.png" alt="Logo Les Rousses" title="Accueil"></a>
+		<h1>Village Les Rousses</h1>
+
+		<nav><ul>
+			<li>
+
+		</ul></nav>
+	</header>
+
+</body>
+</html>
