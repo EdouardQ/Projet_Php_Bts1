dc20220f5fd1efd2f12007cab01685b5c61f9147
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-20 18:56:01 +0100

    Update Reservation_pro.php

--- Pages_site/Reservation_pro.php
+++ Pages_site/Reservation_pro.php
@@ -31,7 +31,7 @@ session_start();
 		</ul></nav>
 	</header>
 	<main>
-		<div><h3>Information :</h3>Les materiels audio et video pourront être demandé à l'accueil.<br>Les reservations professionnelles ne sont pas disponible en période de vacances scolaires.</div>
+		<div><h3>Information :</h3>Le materiel audio et video pourront être demandé à <a href=".\Courage.php" target="blank" id="meme">l'accueil</a>.<br>Les reservations professionnelles ne sont pas disponible en période de vacances scolaires.</div>
 		<form method="post" action="...........">
 			<fieldset id="cadre">
 				<legend><h3>Reservation professionelle</h3></legend>
