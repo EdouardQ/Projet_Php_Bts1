fc1d0302fe506052d25bca50547c2b57dd8060d1
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-22 19:33:54 +0100

    Update Reservation_vac.php

--- Pages_site/Reservation_vac.php
+++ Pages_site/Reservation_vac.php
@@ -63,8 +63,8 @@ include '..\functions.php';
 					?>
 				</select>
 				<span class="form">	Nombre de semaine(s) :</span><input type="number" name="nb_semaine" min=1 style=" width : 40px"></p>
-				<input id="valider" type="submit" name="Valider" value="valider">
-      			<input type="reset" name="Reinitialiser" value="reinitialiser">
+				<input id="valider" type="submit" name="Valider" value="Valider">
+      			<input type="reset" name="Reinitialiser" value="Reinitialiser">
 			</fieldset>
 		</form>
 		<?php
