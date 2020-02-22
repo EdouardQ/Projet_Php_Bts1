03539f41cc4e91811bd7e009fde267114fe44295
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-22 16:30:53 +0100

    main

---
+++ script_date_vacances.php
@@ -0,0 +1,10 @@
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("toussaint 2019",STR_TO_DATE('19,10,2019','%d,%m,%Y') ,STR_TO_DATE('2,11,2019','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("noel 2019",STR_TO_DATE('21,12,2019','%d,%m,%Y') ,STR_TO_DATE('4,1,2020','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("hiver 2020",STR_TO_DATE('8,2,2020','%d,%m,%Y') ,STR_TO_DATE('7,3,2020','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("printemps 2020",STR_TO_DATE('4,4,2020','%d,%m,%Y') ,STR_TO_DATE('2,5,2020','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("ete 2020",STR_TO_DATE('4,7,2020','%d,%m,%Y') ,STR_TO_DATE('29,8,2020','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("toussaint 2020",STR_TO_DATE('17,10,2020','%d,%m,%Y') ,STR_TO_DATE('31,10,2020','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("noel 2020",STR_TO_DATE('19,12,2020','%d,%m,%Y') ,STR_TO_DATE('2,1,2021','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("hiver 2021",STR_TO_DATE('6,2,2021','%d,%m,%Y') ,STR_TO_DATE('6,3,2021','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("printemps 2021",STR_TO_DATE('10,4,2021','%d,%m,%Y') ,STR_TO_DATE('8,5,2021','%d,%m,%Y'));
+insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("ete 2021",STR_TO_DATE('3,7,2021','%d,%m,%Y') ,STR_TO_DATE('28,8,2021','%d,%m,%Y'));
