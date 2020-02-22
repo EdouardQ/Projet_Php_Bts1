45487129894e510daa8b472eb2178f4fed9cb604
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-22 18:58:45 +0100

    main

--- script_date_vacances.php
+++ script_date_vacances.php
@@ -1,10 +1,52 @@
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("toussaint 2019",STR_TO_DATE('19,10,2019','%d,%m,%Y') ,STR_TO_DATE('2,11,2019','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("noel 2019",STR_TO_DATE('21,12,2019','%d,%m,%Y') ,STR_TO_DATE('4,1,2020','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("hiver 2020",STR_TO_DATE('8,2,2020','%d,%m,%Y') ,STR_TO_DATE('7,3,2020','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("printemps 2020",STR_TO_DATE('4,4,2020','%d,%m,%Y') ,STR_TO_DATE('2,5,2020','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("ete 2020",STR_TO_DATE('4,7,2020','%d,%m,%Y') ,STR_TO_DATE('29,8,2020','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("toussaint 2020",STR_TO_DATE('17,10,2020','%d,%m,%Y') ,STR_TO_DATE('31,10,2020','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("noel 2020",STR_TO_DATE('19,12,2020','%d,%m,%Y') ,STR_TO_DATE('2,1,2021','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("hiver 2021",STR_TO_DATE('6,2,2021','%d,%m,%Y') ,STR_TO_DATE('6,3,2021','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("printemps 2021",STR_TO_DATE('10,4,2021','%d,%m,%Y') ,STR_TO_DATE('8,5,2021','%d,%m,%Y'));
-insert into date_vacances (`nom`,`date_debut`,`date_fin`) values ("ete 2021",STR_TO_DATE('3,7,2021','%d,%m,%Y') ,STR_TO_DATE('28,8,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("toussaint 2019", 1, STR_TO_DATE('19,10,2019','%d,%m,%Y'), STR_TO_DATE('26,10,2019','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("toussaint 2019", 2, STR_TO_DATE('26,10,2019','%d,%m,%Y'), STR_TO_DATE('2,11,2019','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("noel 2019", 1, STR_TO_DATE('21,12,2019','%d,%m,%Y'), STR_TO_DATE('28,12,2019','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("noel 2019", 2, STR_TO_DATE('28,12,2019','%d,%m,%Y'), STR_TO_DATE('4,1,2020','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2020", 1, STR_TO_DATE('8,2,2020','%d,%m,%Y'), STR_TO_DATE('15,2,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2020", 2, STR_TO_DATE('15,2,2020','%d,%m,%Y'), STR_TO_DATE('22,2,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2020", 3, STR_TO_DATE('22,2,2020','%d,%m,%Y'), STR_TO_DATE('29,2,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2020", 4, STR_TO_DATE('29,2,2020','%d,%m,%Y'), STR_TO_DATE('7,3,2020','%d,%m,%Y'));
+
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2020", 1, STR_TO_DATE('4,4,2020','%d,%m,%Y'), STR_TO_DATE('11,4,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2020", 2, STR_TO_DATE('11,4,2020','%d,%m,%Y'), STR_TO_DATE('18,4,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2020", 3, STR_TO_DATE('18,4,2020','%d,%m,%Y'), STR_TO_DATE('25,4,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2020", 4, STR_TO_DATE('25,4,2020','%d,%m,%Y'), STR_TO_DATE('2,5,2020','%d,%m,%Y'));
+
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 1, STR_TO_DATE('4,7,2020','%d,%m,%Y'), STR_TO_DATE('11,7,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 2, STR_TO_DATE('11,7,2020','%d,%m,%Y'), STR_TO_DATE('18,7,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 3, STR_TO_DATE('18,7,2020','%d,%m,%Y'), STR_TO_DATE('25,7,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 4, STR_TO_DATE('25,7,2020','%d,%m,%Y'), STR_TO_DATE('1,8,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 5, STR_TO_DATE('1,8,2020','%d,%m,%Y'), STR_TO_DATE('8,8,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 6, STR_TO_DATE('8,8,2020','%d,%m,%Y'), STR_TO_DATE('15,8,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 7, STR_TO_DATE('15,8,2020','%d,%m,%Y'), STR_TO_DATE('22,8,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2020", 8, STR_TO_DATE('22,8,2020','%d,%m,%Y'), STR_TO_DATE('29,8,2020','%d,%m,%Y'));
+
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("toussaint 2020", 1, STR_TO_DATE('17,10,2020','%d,%m,%Y'), STR_TO_DATE('24,10,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("toussaint 2020", 2, STR_TO_DATE('24,10,2020','%d,%m,%Y'), STR_TO_DATE('31,10,2020','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("noel 2020", 1, STR_TO_DATE('19,12,2020','%d,%m,%Y'), STR_TO_DATE('26,12,2020','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("noel 2020", 2, STR_TO_DATE('26,12,2020','%d,%m,%Y'), STR_TO_DATE('2,1,2021','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2021", 1, STR_TO_DATE('6,2,2021','%d,%m,%Y'), STR_TO_DATE('13,2,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2021", 2, STR_TO_DATE('13,2,2021','%d,%m,%Y'), STR_TO_DATE('20,2,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2021", 3, STR_TO_DATE('20,2,2021','%d,%m,%Y'), STR_TO_DATE('27,2,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("hiver 2021", 4, STR_TO_DATE('27,2,2021','%d,%m,%Y'), STR_TO_DATE('6,3,2021','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2021", 1, STR_TO_DATE('10,4,2021','%d,%m,%Y'), STR_TO_DATE('17,4,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2021", 2, STR_TO_DATE('17,4,2021','%d,%m,%Y'), STR_TO_DATE('24,4,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2021", 3, STR_TO_DATE('24,4,2021','%d,%m,%Y'), STR_TO_DATE('1,5,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("printemps 2021", 4, STR_TO_DATE('1,5,2021','%d,%m,%Y'), STR_TO_DATE('8,5,2021','%d,%m,%Y'));
+
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 1, STR_TO_DATE('3,7,2021','%d,%m,%Y'), STR_TO_DATE('10,7,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 2, STR_TO_DATE('17,7,2021','%d,%m,%Y'), STR_TO_DATE('24,7,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 3, STR_TO_DATE('24,7,2021','%d,%m,%Y'), STR_TO_DATE('31,7,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 4, STR_TO_DATE('31,7,2021','%d,%m,%Y'), STR_TO_DATE('28,8,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 5, STR_TO_DATE('3,7,2021','%d,%m,%Y'), STR_TO_DATE('7,8,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 6, STR_TO_DATE('7,8,2021','%d,%m,%Y'), STR_TO_DATE('14,8,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 7, STR_TO_DATE('14,8,2021','%d,%m,%Y'), STR_TO_DATE('21,8,2021','%d,%m,%Y'));
+insert into date_vacances (nom,nb_semaine,date_debut,date_fin) values ("ete 2021", 8, STR_TO_DATE('21,8,2021','%d,%m,%Y'), STR_TO_DATE('28,8,2021','%d,%m,%Y'));
