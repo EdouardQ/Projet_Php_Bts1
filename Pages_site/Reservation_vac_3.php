852617d53c2fb70733215b1348219c885a8f073a
EdouardQ <36738001+EdouardQ@users.noreply.github.com>
2020-02-24 09:28:10 +0100

    main

--- Pages_site/Reservation_vac_3.php
+++ Pages_site/Reservation_vac_3.php
@@ -2,7 +2,7 @@
 session_start();
 setlocale(LC_TIME, "fr_FR");
 include '..\functions.php';
-if ((!isset($_SESSION['date_debut_sejour']) && !isset($_SESSION['date_fin_sejour'])) && strtotime($_POST['date_debut_sejour']) > strtotime($_POST['date_fin_sejour'])){
+if (isset($_POST['date_debut_sejour']) && isset($_POST['date_fin_sejour']) && strtotime($_POST['date_debut_sejour']) < strtotime($_POST['date_fin_sejour'])){
 	$_SESSION['date_debut_sejour']=$_POST['date_debut_sejour'];
 	$_SESSION['date_fin_sejour']=$_POST['date_fin_sejour'];
 }else{
